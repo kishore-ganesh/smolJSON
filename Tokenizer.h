00a8599c2c9356bda5be29465a28375322b8cbc7
kishore-ganesh <kishore2912000@gmail.com>
2020-02-07 00:25:15 +0530

    Fixed number parse
    Signed-off-by: kishore-ganesh <kishore2912000@gmail.com>

--- Tokenizer.h
+++ Tokenizer.h
@@ -1,10 +1,11 @@
-#include<fstream>
-#include<vector>
-#include<map>
-#include<stdexcept>
-#include<stack>
-#include<iostream>
-enum class TOKEN{
+#include <fstream>
+#include <vector>
+#include <map>
+#include <stdexcept>
+#include <stack>
+#include <iostream>
+enum class TOKEN
+{
     CURLY_OPEN,
     CURLY_CLOSE,
     COLON,
@@ -15,143 +16,180 @@ enum class TOKEN{
     COMMA
 };
 
-
-struct Token{
+struct Token
+{
     std::string value;
     TOKEN type;
-    std::string toString(){
-        switch(type){
-            case TOKEN::CURLY_OPEN: {
-                return "Curly open"; 
-            }
-            case TOKEN::CURLY_CLOSE:{
-                return "Curly close";
-            }
-            case TOKEN::COLON:{
-                return "COLON";
-            }
-            case TOKEN::NUMBER:{
-                return "Number: " + value;
-            }
-            case TOKEN::STRING:{
-                return "String: " + value;
-            }
+    std::string toString()
+    {
+        switch (type)
+        {
+        case TOKEN::CURLY_OPEN:
+        {
+            return "Curly open";
+        }
+        case TOKEN::CURLY_CLOSE:
+        {
+            return "Curly close";
+        }
+        case TOKEN::COLON:
+        {
+            return "COLON";
+        }
+        case TOKEN::NUMBER:
+        {
+            return "Number: " + value;
+        }
+        case TOKEN::STRING:
+        {
+            return "String: " + value;
+        }
 
-            case TOKEN::ARRAY_OPEN:{
-                return "Array open";
-            }
-            case TOKEN::ARRAY_CLOSE:{
-                return "Array close";
-            }
-            case TOKEN::COMMA:{
-                return "Comma";
-            }
-
-
+        case TOKEN::ARRAY_OPEN:
+        {
+            return "Array open";
+        }
+        case TOKEN::ARRAY_CLOSE:
+        {
+            return "Array close";
+        }
+        case TOKEN::COMMA:
+        {
+            return "Comma";
+        }
         }
     }
-
 };
 
-class Tokenizer{
+class Tokenizer
+{
     std::fstream file;
     size_t prevPos;
-    public:
-    Tokenizer(std::string fileName){
+
+public:
+    Tokenizer(std::string fileName)
+    {
         file.open(fileName, std::ios::in);
-        if(!file.good()){
+        if (!file.good())
+        {
             std::cout << "File open error" << std::endl;
         }
     }
-    auto getWithoutWhiteSpace(){
+    auto getWithoutWhiteSpace()
+    {
         char c = ' ';
-        while((c==' '||c=='\n')){
+        while ((c == ' ' || c == '\n'))
+        {
             file.get(c); // check
 
-            if((c==' '||c=='\n')&&!file.good()){
+            if ((c == ' ' || c == '\n') && !file.good())
+            {
                 // std::cout << file.eof() << " " << file.fail() << std::endl;
                 throw std::logic_error("Ran out of tokens");
             }
-            else if(!file.good()){
+            else if (!file.good())
+            {
                 return c;
             }
         }
         // std::cout << "NOWHITE: " << c << std::endl;
         return c;
     }
-    auto getToken(){
+    auto getToken()
+    {
         // string buf;
         char c;
-        if(file.eof()){
+        if (file.eof())
+        {
             std::cout << "Exhaused tokens" << std::endl;
             // throw std::exception("Exhausted tokens");
         }
         prevPos = file.tellg();
         c = getWithoutWhiteSpace();
-        
-        
+
         struct Token token;
-        if(c=='"'){
+        if (c == '"')
+        {
             token.type = TOKEN::STRING;
             token.value = "";
             file.get(c);
-            while(c!='"'){
-                token.value+=c;
+            while (c != '"')
+            {
+                token.value += c;
                 file.get(c);
             }
         }
-        else if(c=='{'){
+        else if (c == '{')
+        {
             token.type = TOKEN::CURLY_OPEN;
         }
-        else if(c=='}'){
+        else if (c == '}')
+        {
             token.type = TOKEN::CURLY_CLOSE;
         }
-        else if(c>='0'&&c<='9'){
+        else if (c >= '0' && c <= '9')
+        {
             //Check if string is numeric
             token.type = TOKEN::NUMBER;
             token.value = "";
-            token.value+=c;
-            while((c>='0'&&c<='9')||c=='.'){
+            token.value += c;
+            std::streampos prevCharPos = file.tellg();
+            while ((c >= '0' && c <= '9') || c == '.')
+            {
+                prevCharPos = file.tellg();
                 file.get(c);
-                
-                if(file.eof()){
+
+                if (file.eof())
+                {
                     break;
                 }
-                else{
-                    token.value+=c;
+                else
+                {
+                    if ((c >= '0' && c <= '9')||(c=='.'))
+                    {
+                        token.value += c;
+                    }
+                    else
+                    {
+                        file.seekg(prevCharPos);
+                        // std::cout << c << std::endl;
+                    }
                 }
             }
-
         }
-        else if(c=='[')
+        else if (c == '[')
         {
             token.type = TOKEN::ARRAY_OPEN;
         }
-        else if(c==']')
+        else if (c == ']')
         {
             token.type = TOKEN::ARRAY_CLOSE;
         }
-        else if(c==':'){
+        else if (c == ':')
+        {
             token.type = TOKEN::COLON;
         }
-        else if(c==','){
+        else if (c == ',')
+        {
             token.type = TOKEN::COMMA;
         }
         return token;
     }
 
-    auto hasMoreTokens(){
+    auto hasMoreTokens()
+    {
         return !file.eof();
     }
 
-    void rollBackToken(){
-        if(file.eof()){
+    void rollBackToken()
+    {
+        if (file.eof())
+        {
             file.clear();
         }
         file.seekg(prevPos);
     }
 
-    
     /*
         "": String begin?
         {}: Object
