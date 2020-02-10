464734623ad9665037349714957052307b122d6f
kishore-ganesh <kishore2912000@gmail.com>
2020-02-10 14:56:56 +0530

    Changed to union and refactored to namespace
    Signed-off-by: kishore-ganesh <kishore2912000@gmail.com>

--- JSONNode.h
+++ JSONNode.h
@@ -1,152 +1,118 @@
 #include <map>
-#include <vector>
 #include <memory>
-enum class Type
-{
-    OBJECT,
-    LIST,
-    STRING,
-    NUMBER
-}; // make it private
-class JSONNode
-{
+#include <vector>
 
-    using JSONObject = std::map<std::string, std::shared_ptr<JSONNode>>; 
-    using JSONList = std::vector<std::shared_ptr<JSONNode>>; 
-    union Values{
-    JSONObject object;
-    JSONList list;
-    std::string s;
+namespace JSON {
+class JSONNode;
+using JSONObject = std::map<std::string, std::shared_ptr<JSONNode>>;
+using JSONList = std::vector<std::shared_ptr<JSONNode>>;
+
+class JSONNode {
+  enum class Type { OBJECT, LIST, STRING, NUMBER }; // make it private
+  union Values {
+    JSONObject *object;
+    JSONList *list;
+    std::string *s;
     float value;
-    Values(JSONObject &object){
-        this->object = object;
-    }
-    } values;    
-    Type type;
+  } values;
+  Type type;
 
 public:
-    JSONNode()
-    {
+  JSONNode() {}
+  JSONNode(Type t) { type = t; }
+  auto returnObject() {
+    if (type == Type::OBJECT) {
+      return *values.object;
     }
-    JSONNode(Type t)
-    {
-        type = t;
+    throw std::logic_error("Improper return");
+  }
+  auto returnList() {
+    if (type == Type::LIST) {
+      return *values.list;
     }
-    auto returnObject()
-    {
-        if (type == Type::OBJECT)
-        {
-            return values.object;
-        }
-        throw std::logic_error("Improper return");
+    throw std::logic_error("Improper return");
+  }
+  auto returnString() {
+    if (type == Type::STRING) {
+      return *values.s;
     }
-    auto returnList()
-    {
-        if (type == Type::LIST)
-        {
-            return values.list;
-        }
-        throw std::logic_error("Improper return");
+    throw std::logic_error("Improper return");
+  }
+  auto returnValue() {
+    if (type == Type::NUMBER) {
+      return values.value;
     }
-    auto returnString()
-    {
-        if (type == Type::STRING)
-        {
-            return values.s;
-        }
-        throw std::logic_error("Improper return");
-    }
-    auto returnValue()
-    {
-        if (type == Type::NUMBER)
-        {
-            return values.value;
-        }
-        throw std::logic_error("Improper return");
-    }
+    throw std::logic_error("Improper return");
+  }
 
-    void setObject(JSONObject object) {
-        new (&values.object) JSONObject; 
-        this->values.object = object;
-        type = Type::OBJECT;
-        // if(type==Type::OBJECT){
-        //     this->object = toAdd;
-        // }
-    }
+  void setObject(JSONObject *object) {
+    this->values.object = object;
+    type = Type::OBJECT;
+    // if(type==Type::OBJECT){
+    //     this->object = toAdd;
+    // }
+  }
 
-    void setString(std::string s)
-    {
-        this->values.s = s;
-        type = Type::STRING;
-    }
-    void setNumber(float f)
-    {
-        this->values.value = f;
-        type = Type::NUMBER;
-    }
-    void setList(JSONList list)
-    {
-        new (&values.list) JSONList; 
-        this->values.list = list;
-        type = Type::LIST;
-    }
+  void setString(std::string *s) {
+    this->values.s = s;
+    type = Type::STRING;
+  }
+  void setNumber(float f) {
+    this->values.value = f;
+    type = Type::NUMBER;
+  }
+  void setList(JSONList *list) {
+    this->values.list = list;
+    type = Type::LIST;
+  }
 
-    std::string toString(int indentationLevel)
-    {
-        std::string spaceString = std::string(indentationLevel, ' ');
-        std::string outputString = "";
+  std::string toString(int indentationLevel) {
+    std::string spaceString = std::string(indentationLevel, ' ');
+    std::string outputString = "";
 
-        switch (type)
-        {
-        case Type::STRING:
-        {
-            outputString += spaceString + values.s;
-            break;
-        }
-        case Type::NUMBER:
-        {
-            outputString += spaceString + std::to_string(values.value);
-            break;
-        }
-        case Type::LIST:
-        {
-            std::cout << "[";
-            int index = 0;
-            for (auto node : values.list)
-            {
-                outputString += node->toString(indentationLevel + 1);
-                if (index < list.size() - 1)
-                {
-                    outputString += spaceString + ", ";
-                }
-                index++;
-            }
-            outputString += spaceString + "]\n";
-            break;
-        }
-        case Type::OBJECT:
-        {
-            outputString += "{\n";
-            for (JSONObject::iterator i = values.object.begin(); i != values.object.end(); i++)
-            {
-                outputString += spaceString + i->first + ": ";
-                outputString += i->second->toString(indentationLevel + 1);
-                JSONObject::iterator next = i;
-                next++;
-                if ((next) != object.end())
-                {
-                    outputString += spaceString + ",";
-                }
-                outputString += spaceString + "\n";
-            }
-            outputString += "}\n";
-            return outputString;
-        }
-        }
+    switch (type) {
+    case Type::STRING: {
+      outputString += spaceString + *values.s;
+      break;
     }
+    case Type::NUMBER: {
+      outputString += spaceString + std::to_string(values.value);
+      break;
+    }
+    case Type::LIST: {
+      std::cout << "[";
+      int index = 0;
+      for (auto node : (*values.list)) {
+        outputString += node->toString(indentationLevel + 1);
+        if (index < (*values.list).size() - 1) {
+          outputString += spaceString + ", ";
+        }
+        index++;
+      }
+      outputString += spaceString + "]\n";
+      break;
+    }
+    case Type::OBJECT: {
+      outputString += "{\n";
+      for (JSONObject::iterator i = (*values.object).begin();
+           i != (*values.object).end(); i++) {
+        outputString += spaceString + i->first + ": ";
+        outputString += i->second->toString(indentationLevel + 1);
+        JSONObject::iterator next = i;
+        next++;
+        if ((next) != (*values.object).end()) {
+          outputString += spaceString + ",";
+        }
+        outputString += spaceString + "\n";
+      }
+      outputString += "}\n";
+      return outputString;
+    }
+    }
+  }
 
-    void printNode(int indentationLevel)
-    {
-        std::cout << toString(indentationLevel);
-    }
+  void printNode(int indentationLevel) {
+    std::cout << toString(indentationLevel);
+  }
 };
+} // namespace JSON
--- JSONParser.h
+++ JSONParser.h
@@ -2,206 +2,172 @@
 #include "Tokenizer.h"
 #include <memory>
 
-class JSONParser
-{
-    std::fstream file;
-    std::shared_ptr<JSONNode> root;
-    std::unique_ptr<JSONNode> current;
-    Tokenizer tokenizer;
+class JSONParser {
+  std::fstream file;
+  std::shared_ptr<JSON::JSONNode> root;
+  std::unique_ptr<JSON::JSONNode> current;
+  Tokenizer tokenizer;
 
 public:
-    JSONParser(const std::string filename) : tokenizer(filename) {}
+  JSONParser(const std::string filename) : tokenizer(filename) {}
 
-    void parse()
-    {
-        std::string key = "";
-        while (tokenizer.hasMoreTokens())
-        {
-            Token token;
-            try
-            {
-                token = tokenizer.getToken();
-                std::cout << token.toString() << std::endl;
-                switch (token.type)
-                {
-                case TOKEN::CURLY_OPEN:
-                {
-                    std::shared_ptr<JSONNode> parsedObject = parseObject();
-                    parsedObject->printNode(0);
-                    if (!root)
-                    {
-                        root = parsedObject;
-                    }
-                    break;
-                }
-                case TOKEN::ARRAY_OPEN:
-                {
-                    std::shared_ptr<JSONNode> parsedList = parseList();
-                    parsedList->printNode(0);
-                    if (!root)
-                    {
-                        root = parsedList;
-                    }
-                    break;
-                }
-
-                case TOKEN::STRING:
-                {
-                    tokenizer.rollBackToken();
-                    std::shared_ptr<JSONNode> parsedString = parseString();
-                    parsedString->printNode(0);
-                    if (!root)
-                    {
-                        root = parsedString;
-                    }
-                    break;
-                }
-                case TOKEN::NUMBER:{
-                    tokenizer.rollBackToken();
-                    std::shared_ptr<JSONNode> parsedNumber = parseNumber();
-                    parsedNumber->printNode(0);
-                    if(!root){
-                        root = parsedNumber;
-                    }
-                    break;
-                }
-                }
-            }
-
-            catch (std::logic_error e)
-            {
-                break;
-            }
+  void parse() {
+    std::string key = "";
+    while (tokenizer.hasMoreTokens()) {
+      Token token;
+      try {
+        token = tokenizer.getToken();
+        std::cout << token.toString() << std::endl;
+        switch (token.type) {
+        case TOKEN::CURLY_OPEN: {
+          std::shared_ptr<JSON::JSONNode> parsedObject = parseObject();
+          parsedObject->printNode(0);
+          if (!root) {
+            root = parsedObject;
+          }
+          break;
         }
-        //assert token not valid
-    }
-
-    std::shared_ptr<JSONNode> parseObject()
-    {
-        std::cout << "Parsing object " << std::endl;
-        std::shared_ptr<JSONNode> node = std::make_shared<JSONNode>();
-        std::map<std::string, std::shared_ptr<JSONNode>> keyObjectMap;
-        bool hasCompleted = false;
-        while (!hasCompleted)
-        {
-            if (tokenizer.hasMoreTokens())
-            {
-                Token nextToken = tokenizer.getToken();
-                std::string key = nextToken.value;
-                std::cout << key << std::endl;
-                tokenizer.getToken();
-                nextToken = tokenizer.getToken();
-                std::shared_ptr<JSONNode> node;
-                switch (nextToken.type)
-                {
-                case TOKEN::STRING:
-                {
-                    tokenizer.rollBackToken();
-                    keyObjectMap[key] = parseString();
-                    break;
-                }
-                case TOKEN::ARRAY_OPEN:
-                {
-                    keyObjectMap[key] = parseList();
-                    break;
-                }
-                case TOKEN::NUMBER:
-                {
-                    tokenizer.rollBackToken();
-                    keyObjectMap[key] = parseNumber();
-                    break;
-                }
-                case TOKEN::CURLY_OPEN:
-                {
-                    keyObjectMap[key] = parseObject();
-                    break;
-                }
-                }
-                nextToken = tokenizer.getToken();
-                if (nextToken.type == TOKEN::CURLY_CLOSE)
-                {
-                    hasCompleted = true;
-                    break;
-                }
-            }
-            else
-            {
-                throw std::logic_error("No more tokens");
-            }
+        case TOKEN::ARRAY_OPEN: {
+          std::shared_ptr<JSON::JSONNode> parsedList = parseList();
+          parsedList->printNode(0);
+          if (!root) {
+            root = parsedList;
+          }
+          break;
         }
-        node->setObject(keyObjectMap);
-        return node;
+
+        case TOKEN::STRING: {
+          tokenizer.rollBackToken();
+          std::shared_ptr<JSON::JSONNode> parsedString = parseString();
+          parsedString->printNode(0);
+          if (!root) {
+            root = parsedString;
+          }
+          break;
+        }
+        case TOKEN::NUMBER: {
+          tokenizer.rollBackToken();
+          std::shared_ptr<JSON::JSONNode> parsedNumber = parseNumber();
+          parsedNumber->printNode(0);
+          if (!root) {
+            root = parsedNumber;
+          }
+          break;
+        }
+        }
+      }
+
+      catch (std::logic_error e) {
+        break;
+      }
     }
-    std::shared_ptr<JSONNode> parseString()
-    {
-        std::cout << "Parsing String" << std::endl;
-        std::shared_ptr<JSONNode> node = std::make_shared<JSONNode>();
-        Token token = tokenizer.getToken();
-        node->setString(token.value);
-        return node;
-    }
-    std::shared_ptr<JSONNode> parseNumber()
-    {
-        std::cout << "Parsing Number" << std::endl;
-        std::shared_ptr<JSONNode> node = std::make_shared<JSONNode>();
+    // assert token not valid
+  }
+
+  std::shared_ptr<JSON::JSONNode> parseObject() {
+    std::cout << "Parsing object " << std::endl;
+    std::shared_ptr<JSON::JSONNode> node = std::make_shared<JSON::JSONNode>();
+    JSON::JSONObject *keyObjectMap = new JSON::JSONObject();
+    bool hasCompleted = false;
+    while (!hasCompleted) {
+      if (tokenizer.hasMoreTokens()) {
         Token nextToken = tokenizer.getToken();
-        std::string value = nextToken.value;
-        std::cout << value << std::endl;
-        float fValue = std::stof(value);
-        node->setNumber(fValue);
-        return node;
-    }
-
-    std::shared_ptr<JSONNode> parseList()
-    {
-        std::cout << "Parsing List" << std::endl;
-        std::shared_ptr<JSONNode> node = std::make_shared<JSONNode>();
-        std::vector<std::shared_ptr<JSONNode>> list;
-        bool hasCompleted = false;
-        while (!hasCompleted)
-        {
-            if (!tokenizer.hasMoreTokens())
-            {
-                throw std::logic_error("No more tokens");
-            }
-            else
-            {
-                Token nextToken = tokenizer.getToken();
-                std::shared_ptr<JSONNode> node;
-                switch (nextToken.type)
-                {
-                case TOKEN::ARRAY_OPEN:
-                {
-                    node = parseList();
-                    break;
-                }
-                case TOKEN::CURLY_OPEN:
-                {
-                    node = parseObject();
-                    break;
-                }
-                case TOKEN::STRING:
-                {
-                    tokenizer.rollBackToken();
-                    node = parseString();
-                    break;
-                }
-                case TOKEN::NUMBER:
-                {
-                    tokenizer.rollBackToken();
-                    node = parseNumber();
-                    break;
-                }
-                }
-                list.push_back(node);
-                nextToken = tokenizer.getToken();
-                if (nextToken.type == TOKEN::ARRAY_CLOSE)
-                {
-                    hasCompleted = true;
-                }
-            }
+        std::string key = nextToken.value;
+        std::cout << key << std::endl;
+        tokenizer.getToken();
+        nextToken = tokenizer.getToken();
+        std::shared_ptr<JSON::JSONNode> node;
+        switch (nextToken.type) {
+        case TOKEN::STRING: {
+          tokenizer.rollBackToken();
+          (*keyObjectMap)[key] = parseString();
+          break;
         }
-        node->setList(list);
-        return node;
+        case TOKEN::ARRAY_OPEN: {
+          (*keyObjectMap)[key] = parseList();
+          break;
+        }
+        case TOKEN::NUMBER: {
+          tokenizer.rollBackToken();
+          (*keyObjectMap)[key] = parseNumber();
+          break;
+        }
+        case TOKEN::CURLY_OPEN: {
+          (*keyObjectMap)[key] = parseObject();
+          break;
+        }
+        }
+        nextToken = tokenizer.getToken();
+        if (nextToken.type == TOKEN::CURLY_CLOSE) {
+          hasCompleted = true;
+          break;
+        }
+      } else {
+        throw std::logic_error("No more tokens");
+      }
     }
+    node->setObject(keyObjectMap);
+    return node;
+  }
+  std::shared_ptr<JSON::JSONNode> parseString() {
+    std::cout << "Parsing String" << std::endl;
+    std::shared_ptr<JSON::JSONNode> node = std::make_shared<JSON::JSONNode>();
+    Token token = tokenizer.getToken();
+    std::string *sValue = new std::string(token.value);
+    node->setString(sValue);
+    return node;
+  }
+  std::shared_ptr<JSON::JSONNode> parseNumber() {
+    std::cout << "Parsing Number" << std::endl;
+    std::shared_ptr<JSON::JSONNode> node = std::make_shared<JSON::JSONNode>();
+    Token nextToken = tokenizer.getToken();
+    std::string value = nextToken.value;
+    std::cout << value << std::endl;
+    float fValue = std::stof(value);
+    node->setNumber(fValue);
+    return node;
+  }
+
+  std::shared_ptr<JSON::JSONNode> parseList() {
+    std::cout << "Parsing List" << std::endl;
+    std::shared_ptr<JSON::JSONNode> node = std::make_shared<JSON::JSONNode>();
+    JSON::JSONList *list =
+        new JSON::JSONList();
+    bool hasCompleted = false;
+    while (!hasCompleted) {
+      if (!tokenizer.hasMoreTokens()) {
+        throw std::logic_error("No more tokens");
+      } else {
+        Token nextToken = tokenizer.getToken();
+        std::shared_ptr<JSON::JSONNode> node;
+        switch (nextToken.type) {
+        case TOKEN::ARRAY_OPEN: {
+          node = parseList();
+          break;
+        }
+        case TOKEN::CURLY_OPEN: {
+          node = parseObject();
+          break;
+        }
+        case TOKEN::STRING: {
+          tokenizer.rollBackToken();
+          node = parseString();
+          break;
+        }
+        case TOKEN::NUMBER: {
+          tokenizer.rollBackToken();
+          node = parseNumber();
+          break;
+        }
+        }
+        list->push_back(node);
+        nextToken = tokenizer.getToken();
+        if (nextToken.type == TOKEN::ARRAY_CLOSE) {
+          hasCompleted = true;
+        }
+      }
+    }
+    node->setList(list);
+    return node;
+  }
 };
